05e22777ca45c1d22770fb56ba95303fc9be7bc2
Oleg Ivanov <oleg@exactsoft.net>
2024-06-16 12:37:48 -0400

    removed unused and experimental modules

--- web/modules/custom/notification/js/notification.js
+++
@@ -1,9 +0,0 @@
-( function ($) {
-
-    // Command to replace element
-    Drupal.AjaxCommands.prototype.DeleteNotification = function(ajax, response, status) {
-        if(status === 'success') {
-            $('.view-id-notifications').trigger('RefreshView');
-        }
-    }
-})(jQuery)
