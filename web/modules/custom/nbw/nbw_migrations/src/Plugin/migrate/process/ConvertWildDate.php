2653738a875d8d513eb632991b33446d3813c8a9
Kurucz István <never@nevergone.hu>
2024-06-24 07:25:34 +0200

    convert_wild_date migrate plugin use date & time elements

--- web/modules/custom/nbw/nbw_migrations/src/Plugin/migrate/process/ConvertWildDate.php
+++ web/modules/custom/nbw/nbw_migrations/src/Plugin/migrate/process/ConvertWildDate.php
@@ -13,7 +13,7 @@ use Drupal\migrate\Row;
 /**
  * Provides a convert_wild_date plugin.
  *
- * Convert date string, source data convert with regular expression. Use named caption group: year, mon, day.
+ * Convert date string, source data convert with regular expression. Use named caption group: year, mon, day, hour, min, sec
  * Destination format: @see https://www.php.net/manual/en/datetime.format.php
  *
  * Usage:
@@ -23,7 +23,7 @@ use Drupal\migrate\Row;
  *   bar:
  *     plugin: convert_wild_date
  *     source: something_date_field
- *     from_format: '@(?<mon>\d+)[/-](?<day>\d+)[/-](?<year>\d+)@'
+ *     from_format: '@(?<mon>\d+)[/-](?<day>\d+)[/-](?<year>\d+) (?<hour>\d+):(?<min>\d+):(?<sec>\d+)@'
  *     to_format: 'Y-m-d'
  * @endcode
  *
@@ -35,6 +35,7 @@ final class ConvertWildDate extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property): mixed {
+    $date_elements = ['year', 'mon', 'day', 'hour', 'min', 'sec'];
     if (empty($value) && $value !== '0' && $value !== 0) {
       return '';
     }
@@ -54,23 +55,27 @@ final class ConvertWildDate extends ProcessPluginBase {
     elseif ($regexp_result === 0) {
       throw new MigrateException('form_format pattern does not matches.');
     }
-    foreach (['year', 'mon', 'day'] as $item) {
-      if (!isset($source_date_elements[$item])) {
-        throw new MigrateException("from_format: $item named caption group is missing");
+    $date_parts = [];
+    foreach ($date_elements as $item) {
+      if (isset($source_date_elements[$item])) {
+        if (strlen($source_date_elements[$item]) == 1) {
+          // add leading zero
+          $source_date_elements[$item] = '0' . $source_date_elements[$item];
+        }
+        if ($item === 'year' && strlen($source_date_elements[$item]) == 2) {
+          // y: A two digit representation of a year (which is assumed to be in the range 1970-2069, inclusive.
+          $source_date_elements['year'] = \DateTime::createFromFormat('y', $source_date_elements['year'])->format('Y');
+        }
+        $date_parts[$item] = $source_date_elements[$item];
       }
-      elseif (strlen($source_date_elements[$item]) == 1) {
-        // add leading zero
-        $source_date_elements[$item] = '0' . $source_date_elements[$item];
+      else {
+        $date_parts[$item] = '00';
       }
     }
-    if (strlen($source_date_elements['year']) == 2) {
-      // y: A two digit representation of a year (which is assumed to be in the range 1970-2069, inclusive.
-      $source_date_elements['year'] = \DateTime::createFromFormat('y', $source_date_elements['year'])->format('Y');
-    }
-    // Convert 'Y-m-d' format and convert destination date format, if neccessary.
-    $value = "${source_date_elements['year']}-${source_date_elements['mon']}-${source_date_elements['day']}";
-    if ($to_format !== 'Y-m-d') {
-      $value = DateTimePlus::createFromFormat('Y-m-d', $value)->format($to_format);
+    // Convert 'Y-m-d-H-i-s' format and convert destination date format, if neccessary.
+    $value = implode('-', $date_parts);
+    if ($to_format !== 'Y-m-d-H-i-s') {
+      $value = DateTimePlus::createFromFormat('Y-m-d-H-i-s', $value, ['validate_format' => FALSE])->format($to_format);
     }
     return $value;
   }
