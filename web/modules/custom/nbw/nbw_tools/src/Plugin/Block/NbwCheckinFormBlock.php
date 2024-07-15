6e7aa129632cb84c88ca231a27b8835d6d512e33
Kurucz István <never@nevergone.hu>
2024-07-15 16:49:35 +0200

    generated block and form

---
+++ web/modules/custom/nbw/nbw_tools/src/Plugin/Block/NbwCheckinFormBlock.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\nbw_tools\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a nbw checkin form block.
+ *
+ * @Block(
+ *   id = "nbw_tools_nbw_checkin_form",
+ *   admin_label = @Translation("NBW CheckIn Form"),
+ *   category = @Translation("NBW"),
+ * )
+ */
+final class NbwCheckinFormBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Constructs the plugin instance.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    private readonly FormBuilderInterface $formBuilder,
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
+    return new self(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $build['content'] = [
+      '#markup' => $this->t('It works!'),
+    ];
+    return $build;
+  }
+
+}
